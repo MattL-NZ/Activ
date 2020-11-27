b89adac0f1b10a04318cfe2f4d3d7f5f54c97d1e
Matt List <mattylist@gmail.com>
2020-11-27 23:52:30 +1300

    Updated React folder structure, added Activity interfaces and updated ui.

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -1,24 +1,28 @@
 import React, {Component} from 'react';
-import './App.css';
 import axios from 'axios'
 import Header from 'semantic-ui-react/dist/commonjs/elements/Header/Header';
 import Icon from 'semantic-ui-react/dist/commonjs/elements/Icon/Icon';
 import 'semantic-ui-css/semantic.min.css'
 import List from 'semantic-ui-react/dist/commonjs/elements/List/List';
+import { IActivity } from '../models/activity';
 
-class App extends Component {
+interface IState {
+  activities: IActivity[];
+}
+
+class App extends Component<{}, IState> {
   // When the app first loads, the state is empty
-  state = {
-    values: []
+  readonly state: IState = {
+    activities: []
   }
 
   // We then set the state to an array of two objects
   componentDidMount() {
-    axios.get("http://localhost:5000/api/values").then((response) => {
+    axios.get<IActivity[]>("http://localhost:5000/api/activities").then((response) => {
       console.log(response);
       this.setState({
-        values: response.data
-      })
+        activities: response.data
+      });
     });
   }
 
@@ -30,8 +34,8 @@ class App extends Component {
           <Header.Content>Reactivities</Header.Content>
         </Header>
         <List>
-          {this.state.values.map((value: any) => (
-            <List.Item key={value.id}>{value.name}</List.Item>
+          {this.state.activities.map((activities) => (
+            <List.Item key={activities.id}>{activities.title}</List.Item>
           ))}
         </List>
       </div>
